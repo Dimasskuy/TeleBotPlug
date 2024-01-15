b7e1ee9918aca0cec6b10e5be824b88e6b9942cd
Dimasskuy <freefiremax12d@gmail.com>
2024-01-15 22:31:37 +0000

    Initial commit

---
+++ settings.js
@@ -0,0 +1,21 @@
+/*
+Created By Lann Telegram Bot Plugins
+Register Your Apikey in https://api.betabotz.org
+Replace Your Key with the apikey that you get in your profile.
+*/
+
+module.exports = {
+  ownerUsernames: ["DimassAjaa"],//Your Username
+  ownerNumber: "6282257529886",//Your Number 
+  telegramBotToken: "6584226331:AAGcy1ejjutVBYuK6v8yUDJZPiUPm3QtxFc",//Your Bot Tokens
+  imageUrl: "https://cdn.btch.bz/file/98bce82f9d28682aeee63.jpg",//Thumbnail Url
+  buttonUrl: "https://chat.whatsapp.com/DXPU5F2cePXEaysvcImdUy",//Script Url
+  api: "https://api.betabotz.org",
+  apikey: "aBFHC7jn",//Change Your Apikey
+  mess: {
+    eror: "Internal Server Eror 😵",
+    owner: "Sorry, this command can only be accessed by the owner!",
+    group: "Sorry, this command can only be used within a group!",
+    wait: "Your request is being processed...",
+  },
+};
